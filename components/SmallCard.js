7d33c561a65c6d339d92927696f4dd27c9e1b959
benojacobs <beno.jacobs@hotmail.com>
2022-01-04 16:04:12 +0100

    fix icons

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { render } from "storyblok-rich-text-react-renderer"
 import styles from "../styles/SmallCard.module.scss"
-import { Camera } from 'react-feather';
+import { MapPin, Star, DollarSign } from 'react-feather';
 
 
 const SmallCard = ({ lang, title, short, picture, url, type, price, rating }) => {
@@ -14,9 +14,9 @@ const SmallCard = ({ lang, title, short, picture, url, type, price, rating }) =>
         <div className={styles.title}>
           {title}
         </div>
-        <div className={`${styles.address} ${styles.block}`}><Camera size={12}/><span>Paleisstraat 2</span></div>
-        {rating && <div className={`${styles.rating} ${styles.block}`}><div className={styles.block}><i data-feather="star"></i>Rating</div><span>{rating}/10</span></div>}
-        {price && <div className={`${styles.price} ${styles.block}`}><div className={styles.block}><i data-feather="dollar-sign"></i>Price</div>${price}</div>}
+        <div className={`${styles.address} ${styles.block}`}><MapPin size={12}/><span>Paleisstraat 2</span></div>
+        {rating && <div className={`${styles.rating} ${styles.block}`}><div className={styles.block}><Star size={12}/>Rating</div><span>{rating}/10</span></div>}
+        {price && <div className={`${styles.price} ${styles.block}`}><div className={styles.block}><DollarSign size={12}/>Price</div>${price}</div>}
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin elementum dolor.</p>
         <a href={`/${url}`} className={styles.button}>Details</a>
       </div>
