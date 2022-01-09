7ff212e0ffc5e9b29a2354023f24dad655912464
benojacobs <beno.jacobs@hotmail.com>
2022-01-09 21:57:24 +0100

    experiences styling

--- components/Experience.js
+++ components/Experience.js
@@ -3,6 +3,7 @@ import SbEditable from "storyblok-react"
 import { render } from "storyblok-rich-text-react-renderer"
 import styles from "../styles/Experience.module.scss"
 import SmallCardList from "./SmallCardList"
+import { Calendar } from 'react-feather';
 
 
 
@@ -25,18 +26,27 @@ const Experience = ({ data, level }) => {
   return (
     <SbEditable content={content} key={content._uid}>
       <main>
-        {/* <div className={[styles.movie, styles.test].join(' ')}> */}
-        <div className={styles.newsitem}>
-          <h1 className={styles.title}>
-            {content.title}
-          </h1>
-          <div className={styles.mainpicture} style={{ backgroundImage: `url("${content.image.filename}")` }}>
-          </div>
-          <div className={styles.short}>
-            {render(content.short)}
+        <div className={styles.experience}>
+          <div className={styles.image} style={{ backgroundImage: `url("${content.bannerimage.filename}")` }}></div>
+          <div className={styles.experienceinfo}>
+            <h1 className={styles.title}>
+              {content.title}
+            </h1>
+            <div className={styles.flex}>
+              <div className={styles.avatar} style={{ backgroundImage: `url("${content.image.filename}")` }}></div>
+              <div>
+                <div className={styles.author}>{content.author}</div>
+                <div className={styles.meta}><Calendar size={12}/>{content.date}</div>
+              </div>
+            </div>
           </div>
           <div className={styles.article}>
             {render(content.article)}
+
+            <div className={styles.pills}>
+              <p className={styles.pill}>Likes: {content.likes}</p>
+              <p className={styles.pill}>Reactions: {content.reactions}</p>
+            </div>
           </div>
         </div>
       </main>
