d002466b3a31da91226bc4d25a89f0d2aac5c3a3
Beno Jacobs <beno.jacobs@hotmail.com>
2022-01-13 19:49:51 +0100

    Added hotjar + live-chat

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,8 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>}
+      <iframe width="540" height="305" src="https://d330b8b5.sibforms.com/serve/MUIEADP1V93Oyhpm5toSpywBDOx16aF4nf2IsOyVqlpwTZrD6I4vInY2Vl5zheliKkJmAUr09fNKQ-tdWU8yAXfmtRFllPp9shQ99Tcpu3s2wFeH4M-F5VIAWzHYpWCuqGBHen8YZteChzhwuwtmg6LNoG0Dak3m7Ad_O2rok0Ytm08fMTpI6u7eiEGMWY9liF4iZ4Od4lYoegFv" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>
     </div>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
+    console.log('Check analytics code: ',process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
     return (
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
@@ -21,26 +22,27 @@ export default class MyDocument extends Document {
             });
           `
             }}
-          /> */}
+          />
+         
           {/* <script dangerouslySetInnerHTML={{
             __html: ` (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('${process.env.NEXT_PUBLIC_LEADFEEDER}');`
           }}
           /> */}
 
-          {/* <script dangerouslySetInnerHTML={{
+          <script dangerouslySetInnerHTML={{
             __html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/XXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXX';
+            s1.src='https://embed.tawk.to//61e055fbf7cf527e84d1fdc5/1fpa4voag';
             s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
+            s1.setAttribute('crossOrigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
           }}
-          /> */}
+          />
 
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446177920850529"
-            crossorigin="anonymous"></script>
+            crossOrigin="anonymous"></script>
 
         </Head>
         <body>
